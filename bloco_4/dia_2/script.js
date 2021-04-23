e2f840cf4a1827a2bc8e863a78b9f28131f4ba48
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2021-04-23 19:27:48 -0300

    resolve exercicios de 5 ao 7

--- bloco_4/dia_2/script.js
+++ bloco_4/dia_2/script.js
@@ -8,15 +8,15 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 /*
 for (let number of numbers) {
     console.log(number);
-}*/
+} */
 
 // 02 - Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
 
-let somaNumbers = 0;
+/* let somaNumbers = 0;
 
 for (let number of numbers) {
     somaNumbers += number;
-}
+}*/
 
 //console.log(somaNumbers)
 
@@ -29,12 +29,60 @@ for (let number of numbers) {
 // 04 - Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem:
 // "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
 
-let mediaNumbers = somaNumbers / numbers.length;
+/* let mediaNumbers = somaNumbers / numbers.length;
 
 if (mediaNumbers > 20) {
     console.log("Valor maior que 20");
 }
 else {
     console.log("Valor menor ou igual a 20");
-}
+} */
 
+// 05 - Utilizando for , descubra qual o maior valor contido no array e imprima-o;
+
+/*
+let maiorValor = numbers[0];
+
+for (let index1 = 0; index1 < numbers.length; index1 += 1) {
+    if (numbers[index1] > maiorValor) {  
+        maiorValor = numbers [index1];
+    }
+};
+
+console.log(maiorValor); */
+
+// 06 - Descubra quantos valores ímpares existem no array e imprima o resultado.
+// Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
+
+/*
+let valoresImpares = [];
+let Impar = false ;
+
+for (index = 0; index < numbers.length; index += 1) {
+    if (numbers[index] % 2 !== 0) {
+        valoresImpares.push(numbers[index]);
+    }
+    if (numbers[index] % 2 !== 0) {
+        Impar = true;
+    }
+};
+
+if (Impar === false) {
+    console.log("nenhum valor ímpar encontrado")
+}
+else {
+    console.log(valoresImpares)
+}
+*/
+
+// 07 - Utilizando for , descubra qual o menor valor contido no array e imprima-o;
+
+let menorValor = numbers[0];
+
+for (let index1 = 0; index1 < numbers.length; index1 += 1) {
+    if (numbers[index1] < menorValor) {  
+        menorValor = numbers [index1];
+    };
+};
+
+console.log(menorValor);
