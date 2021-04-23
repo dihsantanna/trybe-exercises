a786bc8bee11e115ed6ff271db9c4e91bf12b8dd
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2021-04-23 16:25:49 -0300

    resolve exercicios de 1 ao 4

---
+++ bloco_4/dia_2/script.js
@@ -0,0 +1,40 @@
+
+// Leia atentamente os enunciados e faça o que se pede! Iremos utilizar esse array para realizar os exercícios do 1 ao 7:
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// 01 - Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log();
+
+/*
+for (let number of numbers) {
+    console.log(number);
+}*/
+
+// 02 - Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
+
+let somaNumbers = 0;
+
+for (let number of numbers) {
+    somaNumbers += number;
+}
+
+//console.log(somaNumbers)
+
+// 03 - Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
+
+// A média aritmética é o resultado da soma de todos os elementos divido pelo número total de elementos.
+
+// console.log(somaNumbers / numbers.length);
+
+// 04 - Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem:
+// "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
+
+let mediaNumbers = somaNumbers / numbers.length;
+
+if (mediaNumbers > 20) {
+    console.log("Valor maior que 20");
+}
+else {
+    console.log("Valor menor ou igual a 20");
+}
+
