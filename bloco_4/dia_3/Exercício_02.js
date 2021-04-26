744d4f59e0dffe9df2c2f8232567e4c3b4c465c2
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2021-04-26 20:22:30 -0300

    resolve exercicio 02

--- bloco_4/dia_3/Exercício_02.js
+++ bloco_4/dia_3/Exercício_02.js
@@ -10,3 +10,12 @@ n = 5
 *****
 
 */
+
+let n = 5;
+let symbol = "*";
+let imputLine = "";
+
+for (let index = 0; index <= n; index += 1) { 
+    console.log(imputLine);
+    imputLine = imputLine + symbol;
+}
