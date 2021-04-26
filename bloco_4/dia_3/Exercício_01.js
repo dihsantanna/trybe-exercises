49da7d909cf46de9989013c6907b17d77abbd4f1
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2021-04-26 19:49:35 -0300

    resolve exercicio 01

---
+++ bloco_4/dia_3/Exercício_01.js
@@ -0,0 +1,26 @@
+// 1- Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n . Por exemplo:
+/*
+
+n = 5
+
+*****
+*****
+*****
+*****
+*****
+
+*/
+
+let n = 2;
+let symbol = "*";
+let imputLine = "";
+
+if (n > 1) {
+    for (let index = 0; index < n; index += 1) {
+        imputLine = imputLine + symbol;
+    }
+
+    for (let index = 0; index < n; index += 1) {
+        console.log(imputLine);
+    }
+}
