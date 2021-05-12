5492b123e9ec39daaaa02f7dfffd23ae97e9ebd9
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2021-05-12 19:37:13 -0300

    adiciona novas funçoes de verificaçao de dados

--- bloco_6/dia_01/script.js
+++ bloco_6/dia_01/script.js
@@ -7,13 +7,16 @@ for (let index = 0; index < states.length; index += 1) {
     option.innerText = state;
     document.getElementById('select-state').appendChild(option);
 }
-// Validador de data
-document.querySelector('#button-submit').addEventListener('click', () => {
+// Validadores:
+const btnSubmit = document.querySelector('#button-submit');
+function dateValidate() {
     const initialDate = document.querySelector('#initial-date');
     const date = initialDate.value
-    switch (date.length !== 10) {
-        case true:
-       return alert('Verifique formato da data de inicio');
+    if (date.length === 0) {
+        return alert('Preencha o campo Data de início.')
+    }
+    if (date.length !== 10) {
+        return alert('Verifique formato da data de inicio.');
     }
     let day = '';
     let month = '';
@@ -31,13 +34,68 @@ document.querySelector('#button-submit').addEventListener('click', () => {
         }
     }
     if (day < 1 || day > 31) {
-        return alert('Dia da data de inicio inválido');
+        return alert('Dia da data de inicio inválido.');
     } else if (format !== '//') {
-        return alert('Verifique formato da data de inicio');
+        return alert('Verifique formato da data de inicio.');
     } else if (month < 1 || month > 12) {
-        return alert('Mês da data de inicio inválido');
+        return alert('Mês da data de inicio inválido.');
     } else if (year < 0) {
-        return alert('Ano da data de inicio inválido');
+        return alert('Ano da data de inicio inválido.');
+    } else {
+        return true;
     }
-    });
-
+}
+function nameValidate() {
+    const inputName = document.querySelector('#full-name');
+    const fullName = inputName.value
+    if (fullName.length === 0) {
+        return alert('Preencha o campo Nome.')
+    }
+    return true
+}
+function emailValidate() {
+    const inputEmail = document.querySelector('#input-email');
+    const email = inputEmail.value
+    if (email.length === 0) {
+        return alert('Preencha o campo E-mail.')
+    }
+}
+function cpfValidate() {
+    const inputCpf = document.querySelector('#input-cpf');
+    const cpf = inputCpf.value
+    if (cpf.length === 0) {
+        return alert('Preencha o campo CPF.')
+    }
+}
+function addressValidate() {
+    const inputAddress = document.querySelector('#input-address');
+    const address = inputAddress.value
+    if (address.length === 0) {
+        return alert('Preencha o campo Endereço.')
+    }
+}
+function cityValidate() {
+    const inputCity = document.querySelector('#input-city');
+    const city = inputCity.value
+    if (city.length === 0) {
+        return alert('Preencha o campo Cidade.')
+    }
+}
+function curriculumSummaryValidate() {
+    const inputCurriculumSummary = document.querySelector('#curriculum-summary');
+    const curriculumSummary = inputCurriculumSummary.value
+    if (curriculumSummary.length === 0) {
+        return alert('Preencha o campo Resumo do Currículo.')
+    }
+}
+function officeValidate() {
+    const inputOffice = document.querySelector('#input-office');
+    const office = inputOffice.value
+    if (office.length === 0) {
+        return alert('Preencha o campo Cargo.')
+    }
+}
+/*btnSubmit.addEventListener('click', (event) => {
+    event.preventDefault();
+    if 
+});*/
