65961ba56b17c6778c2755ccfa0069f48de057d2
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2021-05-12 21:50:47 -0300

    insere framework CSS bootstrap e inicia nova estilizaçao

--- bloco_6/dia_01_02/script.js
+++ bloco_6/dia_01_02/script.js
@@ -1,5 +1,5 @@
 // Adiciona estados dinamicamente ao campo de estados do form
-const states = ["Acre" , "Alagoas" , "Amapá" , "Amazonas" , "Bahia" , "Ceará" , "Distrito Federal" , "Espírito Santo" , "Goiás" , "Maranhão" , "Mato Grosso" , "Mato Grosso do Sul" , "Minas Gerais" , "Pará" , "Paraíba" , "Paraná" , "Pernambuco" , "Piauí" , "Rio de Janeiro" , "Rio Grande do Norte" , "Rio Grande do Sul" , "Rondônia" , "Roraima" , "Santa Catarina" , "São Paulo" , "Sergipe" , "Tocantins"];
+const states = ['AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO'];
 for (let index = 0; index < states.length; index += 1) {
     const state = states[index]
     const option = document.createElement('option');
