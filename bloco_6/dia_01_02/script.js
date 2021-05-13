47d2193e81e49cdd6d560a586045593b3ac34345
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2021-05-13 00:18:39 -0300

    adiciona bibliotecas pikaday e moment

--- bloco_6/dia_01_02/script.js
+++ bloco_6/dia_01_02/script.js
@@ -1,3 +1,24 @@
+let picker = new Pikaday({
+    field: document.getElementById('datepicker'),
+    format: 'DD/MMM/YYYY',
+    toString(date, format) {
+        // você deve fazer a formatação com base no formato passado, 
+        // mas retornaremos apenas 'DD/MM/AAAA' para simplificar 
+        const day = date.getDate();
+        const month = date.getMonth() + 1;
+        const year = date.getFullYear();
+        return `${day}/${month}/${year}`;
+    },
+    parse(dateString, format) {
+        // dateString é o resultado do método `toString`
+        const parts = dateString.split('/');
+        const day = parseInt(parts[0], 10);
+        const month = parseInt(parts[1], 10) - 1;
+        const year = parseInt(parts[2], 10);
+        return new Date(year, month, day);
+    }
+});
+const initialDate = document.querySelector('#initial-date');
 // Adiciona estados dinamicamente ao campo de estados do form
 const states = ["Escolha..." , "Acre" , "Alagoas" , "Amapá" , "Amazonas" , "Bahia" , "Ceará" , "Distrito Federal" , "Espírito Santo" , "Goiás" , "Maranhão" , "Mato Grosso" , "Mato Grosso do Sul" , "Minas Gerais" , "Pará" , "Paraíba" , "Paraná" , "Pernambuco" , "Piauí" , "Rio de Janeiro" , "Rio Grande do Norte" , "Rio Grande do Sul" , "Rondônia" , "Roraima" , "Santa Catarina" , "São Paulo" , "Sergipe" , "Tocantins"];
 for (let index = 0; index < states.length; index += 1) {
@@ -8,43 +29,6 @@ for (let index = 0; index < states.length; index += 1) {
     document.getElementById('select-state').appendChild(option);
 }
 // Validadores:
-const btnSubmit = document.querySelector('#button-submit');
-function dateValidate() {
-    const initialDate = document.querySelector('#initial-date');
-    const date = initialDate.value
-    if (date.length === 0) {
-        return alert('Preencha o campo Data de início.')
-    }
-    if (date.length !== 10) {
-        return alert('Verifique formato da data de inicio.');
-    }
-    let day = '';
-    let month = '';
-    let year = '';
-    let format = '';
-    for (let index = 0; index < date.length; index += 1) {
-        if (index < 2) {
-            day += date[index];
-        } else if (index === 3 || index === 4) {
-            month += date[index];
-        } else if (index > 5) {
-            year += date[index];
-        } else {
-            format += date[index];
-        }
-    }
-    if (day < 1 || day > 31) {
-        return alert('Dia da data de inicio inválido.');
-    } else if (format !== '//') {
-        return alert('Verifique formato da data de inicio.');
-    } else if (month < 1 || month > 12) {
-        return alert('Mês da data de inicio inválido.');
-    } else if (year < 0) {
-        return alert('Ano da data de inicio inválido.');
-    } else {
-        return true;
-    }
-}
 function nameValidate() {
     const inputName = document.querySelector('#full-name');
     const fullName = inputName.value
