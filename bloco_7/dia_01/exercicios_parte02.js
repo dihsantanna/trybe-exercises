24dc088f83bead9cea6fd1f31f23ad3ae0906b6e
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2021-05-19 20:45:07 -0300

    resolvido exercicio 2

--- bloco_7/dia_01/exercicios_parte02.js
+++ bloco_7/dia_01/exercicios_parte02.js
@@ -1,12 +1,27 @@
-const fatorial = n => {
-    let answer = n;
-    if (n > 0) {
-        for (let index = n - 1; index > 0; index -= 1) {
-            answer = answer * index;
-        }
-    } else {
-        return `Não é possível calcular fatorial de ${n}.`
-    }
-    return answer;
+// Exercício 01
+// const fatorial = n => {
+//     let answer = n;
+//     if (n > 0) {
+//         for (let index = n - 1; index > 0; index -= 1) {
+//             answer = answer * index;
+//         }
+//     } else {
+//         return `Não é possível calcular fatorial de ${n}.`
+//     }
+//     return answer;
+// }
+// console.log(fatorial(4));
+// Exercício 02
+const longestWord = str => {
+   let words = str.split(' ');
+   let maxLength = 0;
+   let answer = '';
+   for (const word of words) {
+       if (word.length > maxLength) {
+        maxLength = word.length
+        answer = word;
+       } 
+   }
+   return answer;
 }
-console.log(fatorial(4));
+console.log(longestWord('Antônio foi no banheiro e não sabemos o que aconteceu'));
