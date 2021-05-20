cb012942135fa81ef11afd93c71a3a82559d040c
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2021-05-19 22:12:15 -0300

    resolve exercicio 03

--- bloco_7/dia_01/exercicios_parte02.js
+++ bloco_7/dia_01/exercicios_parte02.js
@@ -1,16 +1,16 @@
 // Exercício 01
-// const fatorial = n => {
-//     let answer = n;
-//     if (n > 0) {
-//         for (let index = n - 1; index > 0; index -= 1) {
-//             answer = answer * index;
-//         }
-//     } else {
-//         return `Não é possível calcular fatorial de ${n}.`
-//     }
-//     return answer;
-// }
-// console.log(fatorial(4));
+const fatorial = n => {
+    let answer = n;
+    if (n > 0) {
+        for (let index = n - 1; index > 0; index -= 1) {
+            answer = answer * index;
+        }
+    } else {
+        return `Não é possível calcular fatorial de ${n}.`
+    }
+    return answer;
+}
+console.log(fatorial(4));
 // Exercício 02
 const longestWord = str => {
    let words = str.split(' ');
@@ -25,3 +25,12 @@ const longestWord = str => {
    return answer;
 }
 console.log(longestWord('Antônio foi no banheiro e não sabemos o que aconteceu'));
+// Exercício 03
+const btnCounter = document.querySelector('button');
+const counter = document.querySelector('#counter');
+let clickCount = 0;
+counter.innerHTML = clickCount;
+btnCounter.addEventListener('click', () => {
+  clickCount += 1;
+  counter.innerHTML = clickCount;
+});
