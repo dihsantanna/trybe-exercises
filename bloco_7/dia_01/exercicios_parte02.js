dbbf92dba2e9a3cf70679065960b9e9dfb22658a
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2021-05-19 22:58:13 -0300

    resolve exercicio 04 e cumpre todos os exercicios do dia

--- bloco_7/dia_01/exercicios_parte02.js
+++ bloco_7/dia_01/exercicios_parte02.js
@@ -10,7 +10,7 @@ const fatorial = n => {
     }
     return answer;
 }
-console.log(fatorial(4));
+// console.log(fatorial(4));
 // Exercício 02
 const longestWord = str => {
    let words = str.split(' ');
@@ -24,7 +24,7 @@ const longestWord = str => {
    }
    return answer;
 }
-console.log(longestWord('Antônio foi no banheiro e não sabemos o que aconteceu'));
+// console.log(longestWord('Antônio foi no banheiro e não sabemos o que aconteceu'));
 // Exercício 03
 const btnCounter = document.querySelector('button');
 const counter = document.querySelector('#counter');
@@ -34,3 +34,24 @@ btnCounter.addEventListener('click', () => {
   clickCount += 1;
   counter.innerHTML = clickCount;
 });
+// Exercício 04
+// Função 01
+const replaceX = str => {
+  let strDetermined = "Tryber x aqui!";
+  let strEdited = strDetermined.replace('x', str);
+  return strEdited;
+}
+// console.log(replaceX('Diogo'));
+// Função 02
+const myMainSkills = ['JavaScript', 'HTML', 'CSS', 'DOM', 'GIT'];
+const myDescription = (str, arr) => {
+  const sortArr = arr.sort();
+  return `${str} Minhas cinco principais habilidades são:
+  - ${sortArr[0]};
+  - ${sortArr[1]};
+  - ${sortArr[2]};
+  - ${sortArr[3]};
+  - ${sortArr[4]}. #goTrybe`;
+}
+
+console.log(myDescription(replaceX('Diogo'), myMainSkills));
