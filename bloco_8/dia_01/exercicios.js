1aa17542e552a4368aceb07bf154e975f7f6dadd
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2021-05-25 20:15:14 -0300

    resolve exercicio 03 - encerrado exercicios primcipais

--- bloco_8/dia_01/exercicios.js
+++ bloco_8/dia_01/exercicios.js
@@ -16,6 +16,7 @@ const newEmployees = () => {
     }
     return employees;
   };
+
 //   console.log(newEmployees());
 
 // 2 - Desenvolva uma HOF que retorna o resultado de um sorteio. Esta HOF irá gerar um número aleatório entre 1 e 5 recebendo como parâmetros o número apostado e uma função que checa se o número apostado é igual ao número sorteado. O retorno da sua HOF deve ser uma string (Ex: "Tente novamente" ou "Parabéns você ganhou").
@@ -29,3 +30,26 @@ const drawnResult = luckyNum => {
     
 
 // console.log(drawnResult(4));
+
+// 3 - Crie uma HOF que receberá três parâmetros. O primeiro será um array de respostas corretas (Gabarito), o segundo será um array de respostas a serem verificadas (respostas da pessoa estudante) e o terceiro é uma função que checa se as respostas estão corretas e faz a contagem da pontuação final recebida pela pessoa estudante. Ao final a HOF deve retornar o total da contagem de respostas certas.
+// Quando a resposta for correta a contagem sobe 1 ponto, quando for incorreta desce 0.5 pontos, e quando não houver resposta ("N.A") não altera-se a contagem.
+const rightAnswers = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const studentAnswers = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+const score = (template, answers) => {
+    let counter = 0;
+    for (let index = 0; index < template.length; index += 1) {
+        if (answers[index] === template[index]) {
+            counter += 1;
+        } else if(answers[index] !== template[index] & answers[index] !== 'N.A') {
+            counter -= 0.5;
+        }
+    }
+    return `Sua nota é: ${counter}`;
+}
+
+const checkAnswers = (template, answers, callback) => {
+    return callback(template, answers);
+}
+
+// console.log(checkAnswers(rightAnswers, studentAnswers, score));
