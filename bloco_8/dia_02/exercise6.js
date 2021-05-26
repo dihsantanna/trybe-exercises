6a81345422d4cd2a2c81d54df557f7d9a7ec6e1a
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2021-05-26 20:36:29 -0300

    realiza exercicio 06

--- bloco_8/dia_02/exercise6.js
+++ bloco_8/dia_02/exercise6.js
@@ -6,6 +6,7 @@ const expectedResult = true;
 
 function someBookWasReleaseOnThe80s() {
   // escreva seu código aqui
+  return books.some(book => (book.releaseYear >= 1980 && book.releaseYear <= 1989) ? true : false);
 }
 
 assert.strictEqual(someBookWasReleaseOnThe80s(), expectedResult);
