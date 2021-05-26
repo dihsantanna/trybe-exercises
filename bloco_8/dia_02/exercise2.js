176e043192598ee269f12a54373763cbd647d1f5
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2021-05-26 20:11:47 -0300

    realiza exercicio 02

--- bloco_8/dia_02/exercise2.js
+++ bloco_8/dia_02/exercise2.js
@@ -6,9 +6,13 @@ const books = require('./books');
 function smallerName() {
     let nameBook;
     // escreva aqui o seu código
-  
+    books.forEach((book) => {
+      if (!nameBook || book.name.length < nameBook.length) {
+        nameBook = book.name;
+      }
+    });  
     // Variável nameBook que receberá o valor do menor nome;
     return nameBook;
-  }
+}
   
   assert.strictEqual(smallerName(), 'Duna');
