d08d7a53bc54d2cd3b0043d64ae808bdb115ea21
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2021-05-26 20:20:20 -0300

    realiza exercicio 04

--- bloco_8/dia_02/exercise4.js
+++ bloco_8/dia_02/exercise4.js
@@ -49,6 +49,7 @@ const expectedResult = [
   
   function booksOrderedByReleaseYearDesc() {
     // escreva aqui seu código
+    return books.sort((a, b) => b.releaseYear - a.releaseYear);
   }
   
   assert.deepStrictEqual(booksOrderedByReleaseYearDesc(), expectedResult);
