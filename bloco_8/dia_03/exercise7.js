a2bb4cb58f0c3c88dc6a5fbba2f7bfb0396d64d4
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2021-05-27 16:10:07 -0300

    adiciona diretorios e arquivos a para realizaçao dos exercicios do dia 8.3

---
+++ bloco_8/dia_03/exercise7.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+const books = require('./books');
+
+// 7 - Encontre o nome do livro escrito pela pessoa cujo nome registrado começa com três iniciais.
+
+// Dica: cada inicial termina com um ponto.
+
+const expectedResult = 'O Senhor dos Anéis';
+
+function authorWith3DotsOnName() {
+  // escreva seu código aqui
+}
+
+assert.deepStrictEqual(authorWith3DotsOnName(), expectedResult);
