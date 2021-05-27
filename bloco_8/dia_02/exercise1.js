3f00b499344c87985fd5b502e702dbca87876b63
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2021-05-27 15:49:10 -0300

    corrige erros de identaçao e gramatica

--- bloco_8/dia_02/exercise1.js
+++ bloco_8/dia_02/exercise1.js
@@ -2,11 +2,11 @@ const assert = require('assert');
 const books = require('./books');
 
 // 1 - Encontre o nome da primeira pessoa autora do livro nascida no ano de 1947.
-// Dica: use a função find .
+// Dica: use a função find.
 function authorBornIn1947() {
     // escreva aqui o seu código
     return books.find((book) => book.author.birthYear === 1947).author.name;
-  }
+}
   
   assert.strictEqual(authorBornIn1947(), 'Stephen King');
 // console.log(authorBornIn1947())
