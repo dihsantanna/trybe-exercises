e47f13a1b22014a84fcde072ce3c089efcd33684
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2021-05-28 18:50:42 -0300

    resolve exercicio 01

--- bloco_8/dia_04/exercise1.js
+++ bloco_8/dia_04/exercise1.js
@@ -8,7 +8,10 @@ const arrays = [
 ];
 
 function flatten() {
-  // escreva seu código aqui
+return arrays.reduce((acc, currentValue) => {
+        currentValue.forEach((item) => acc.push(item));
+        return acc;
+    }, [])
 }
 
 assert.deepStrictEqual(flatten(), ['1', '2', '3', true, 4, 5, 6]);
