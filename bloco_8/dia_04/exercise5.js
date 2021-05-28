31dc4d0f59f4b6b73c1ff489896f54ad6cf69192
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2021-05-28 20:43:57 -0300

    resolve exercicio 05

--- bloco_8/dia_04/exercise5.js
+++ bloco_8/dia_04/exercise5.js
@@ -1,8 +1,7 @@
 const assert = require('assert');
+const books = require('./books');
 
 // 5 - Dada o array de nomes, retorne a quantidade de vezes em que aparecem a letra a maiúscula ou minúscula.
-const assert = require('assert');
-
 const names = [
   'Aanemarie', 'Adervandes', 'Akifusa',
   'Abegildo', 'Adicellia', 'Aladonata',
@@ -10,7 +9,18 @@ const names = [
 ];
 
 function containsA() {
-  // escreva seu código aqui
+  const counterA = names.reduce((acc, currentValue) => {
+    const wordLower = currentValue.toLocaleLowerCase();
+    const wordArr = wordLower.split('');
+    acc += wordArr.reduce((counter, letter) => {
+      if (letter === 'a') {
+        counter += 1;
+      }
+      return counter;
+    }, 0)
+    return acc;
+  }, 0)
+  return counterA;
 }
 
 assert.deepStrictEqual(containsA(), 20);
