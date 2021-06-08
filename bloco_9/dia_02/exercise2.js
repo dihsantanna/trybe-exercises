2b21084b3cd96d3c5b859685893c275c689e7146
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2021-06-08 19:12:03 -0300

    realiza exercicio 02

---
+++ bloco_9/dia_02/exercise2.js
@@ -0,0 +1,12 @@
+const promise = new Promise((resolve, reject) => {
+    const arr = [];
+        for (let index = 0; index < 10; index += 1) {
+            const num = Math.ceil(Math.random() * (50 - 1) + 1)
+            arr.push(Math.pow(num, 2));
+        }
+    const sumArr = arr.reduce((acc, num) => (acc + num))
+    console.log(sumArr);
+    (sumArr < 8000) ? resolve() : reject();
+})
+.then(() => console.log('Promise resolvida'))
+.catch((error) => console.log('Promise rejeitada'));
