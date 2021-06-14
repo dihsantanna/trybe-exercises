02a77eb3bb23292320fcd5dcf12a5502b4d065b6
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2021-06-14 18:59:57 -0300

    resolve exercicio 02 da parte 02

---
+++ bloco_10/dia_01/Exercicios_parte02_2.js
@@ -0,0 +1,10 @@
+const techList = (techs, firstName) => {
+    if (techs.length === 0) {
+      return 'Vazio!';
+    }
+    const sortTechs = techs.sort();
+    return sortTechs.map((item) => ({ tech: item, name: firstName }));
+  };
+
+  module.exports = techList;
+  
