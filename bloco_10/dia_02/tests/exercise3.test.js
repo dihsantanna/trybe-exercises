457a6e7067935c9c75b140d41ad4fe3bb566f2a4
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2021-06-15 19:54:59 -0300

    Realiza Exercicio 03

--- bloco_10/dia_02/tests/exercise3.test.js
+++ bloco_10/dia_02/tests/exercise3.test.js
@@ -3,3 +3,27 @@ const getUserName = require('../users');
 
 // 3 - Reescreva o teste do exercício anterior, desta vez utilizando a sintaxe de async/await .
 // Dica: Utilize o try/catch para o caso de erro.
+
+describe('Verifica a função getUserName', () => {
+    it('Verifica se getUserName é uma função', () => {
+        expect(typeof getUserName).toBe('function');
+    });
+
+    it('Verifica se getUserName é definida', () => {
+        expect(getUserName).toBeDefined();
+    });
+
+    it('Ao passar a id: 4 como parâmetro retorne "Mark"', async() => {
+        const response = await getUserName(4)
+        expect(response).toBe('Mark');
+    });
+
+    it('Ao passar a id: 6 como parâmetro retorne o objeto { error: "User with 6 not found." }', async() => {
+        expect.assertions(1);
+        try {
+           await getUserName(6);
+        } catch(error) {
+            expect(error).toStrictEqual({ error: "User with 6 not found." });
+        }
+    });
+})
