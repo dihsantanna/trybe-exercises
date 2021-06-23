1c36499da6ccd25dc8f57a9713b0c7dd87e4e2ef
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2021-06-23 01:19:17 -0300

    estiliza pokedex

--- bloco_11/dia_02/my-pokedex/src/Pokedex.js
+++ bloco_11/dia_02/my-pokedex/src/Pokedex.js
@@ -2,11 +2,13 @@ import Pokemon from './Pokemon';
 import pokemons from './data';
 import './App.css';
 
+const TITLE = 'https://fontmeme.com/permalink/210623/d28472afb9a391904f9eab67724b5431.png'
+
 function Pokedex() {
     return (
-        <div>
-            <header>
-                <h1 className="title">Pokedex</h1>
+        <div className="main">
+            <header className="title">
+                <img src={TITLE} alt="Pokedex-Title"/>
             </header>
             <section className="pokedex">
                 {pokemons.map(({ name, type, averageWeight, image, id }) => {
