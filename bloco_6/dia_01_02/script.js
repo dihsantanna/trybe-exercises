45e0e37e0ff598e1163c8afc380584e3037efe84
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2021-06-24 13:50:21 -0300

    atualiza arquivos

---
+++ bloco_6/dia_01_02/script.js
@@ -0,0 +1,334 @@
+const initialDate = document.querySelector('#datepicker');
+// Adiciona estados dinamicamente ao campo de estados do form
+const states = ["" , "Acre" , "Alagoas" , "Amapá" , "Amazonas" , "Bahia" , "Ceará" , "Distrito Federal" , "Espírito Santo" , "Goiás" , "Maranhão" , "Mato Grosso" , "Mato Grosso do Sul" , "Minas Gerais" , "Pará" , "Paraíba" , "Paraná" , "Pernambuco" , "Piauí" , "Rio de Janeiro" , "Rio Grande do Norte" , "Rio Grande do Sul" , "Rondônia" , "Roraima" , "Santa Catarina" , "São Paulo" , "Sergipe" , "Tocantins"];
+for (let index = 0; index < states.length; index += 1) {
+    const state = states[index]
+    const option = document.createElement('option');
+    option.value = state;
+    option.innerText = state;
+    document.getElementById('select-state').appendChild(option);
+}
+// Validadores:
+const picker = new Pikaday({
+    field: document.getElementById('datepicker'),
+    format: 'DD/MM/YYYY',
+    maxDate: moment().toDate(),
+    i18n: {
+        previousMonth : 'Mês Anterior',
+        nextMonth     : 'Próximo Mês',
+        months        : ['Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'],
+        weekdays      : ['Domingo','Segunda','Terça','Quarta','Quinta','Sexta','Sábado'],
+        weekdaysShort : ['Dom','Seg','Ter','Qua','Qui','Sex','Sáb']
+    },
+});
+new window.JustValidate('.js-form', {
+    rules: {
+        name: {
+            required: true,
+            minLength: 5,
+            maxLength: 40,
+        },
+        email: {
+            required: true,
+            email: true,
+        },
+        cpf: {
+            required: true,
+            maxLength: 11,
+            minLength: 11,
+            strength: {
+                custom: /\d\d\d\d\d\d\d\d\d\d\d$/,
+            }
+        },
+        address: {
+            required: true,
+            maxLength: 200,
+            minLength: 10
+        },
+        radio: {
+            required: true,
+        },
+        city: {
+            required: true,
+            minLength: 4,
+            maxLength: 28,
+        },
+        state: {
+            required: true,
+            minLength: 1,
+        },
+
+        text: {
+            required: true,
+            maxLength: 1000,
+            minLength: 50,
+        },
+        text2: {
+            required: true,
+            maxLength: 40,
+            minLength: 4,
+        },
+        text3: {
+            required: true,
+            minLength: 25,
+            maxLength: 500,
+        },
+        date: {
+            required: true,
+            minLength: 10,
+            maxLength: 10,
+            strength: {
+                custom: /\d\d\/\d\d\/\d\d\d\d$/,
+            } 
+        },
+        messages: {
+            name: {
+              minLength: 'Insira no mínimo 5 caracteres!',
+              maxLength: 'Numero máximo de 40 caracteres excedido!',
+              required: 'Campo Nome Obrigatório'
+            },
+            email: 'My custom message about error (one error message for all rules)'
+        },
+        tooltip: {
+            fadeOutTime: 4000, // default value - 5000
+            fadeOutClass: '.hide', // default value - just-validate-tooltip-hide 
+            selectorWrap: '.tooltip-wrapper', // default value - just-validate-tooltip-container
+            colorWrong: 'red',
+        }
+    }
+});
+// const inputName = document.querySelector('#name');
+// function nameValidate() {
+//     const fullName = inputName.value;
+//     if (fullName.length === 0) {
+//         inputName.classList.remove('correct')
+//         inputName.classList.add('erro');
+//         inputName.setAttribute('placeholder', '* Campo obrigatório');
+//         return false;
+//     } else if (fullName.length > 40) {
+//         inputName.classList.remove('correct')
+//         inputName.classList.add('erro');
+//         alert('Excedido o número de caracteres');
+//         return false;
+//     } else if (fullName.length < 7) {
+//         return false;
+//     } else {
+//         inputName.classList.remove('erro');
+//         inputName.classList.add('correct');
+//         return true;
+//     }
+// }
+// const inputEmail = document.querySelector('#email');
+// function emailValidate() {
+//     const email = inputEmail.value;
+//     if (email.length === 0) {
+//         inputEmail.classList.remove('correct')
+//         inputEmail.classList.add('erro');
+//         inputEmail.setAttribute('placeholder', '* Campo obrigatório');
+//         return false;
+//     } else if (email.length > 50) {
+//         inputEmail.classList.remove('correct')
+//         inputEmail.classList.add('erro');
+//         alert('Excedido o número de caracteres');
+//         return false;
+//     } else if (email.indexOf('@') === -1 || email.length < 3) {
+//         return false;
+//     } else {
+//         inputEmail.classList.remove('erro');
+//         inputEmail.classList.add('correct');
+//         return true;
+//     }
+// }
+// const inputCpf = document.querySelector('#input-cpf');
+// function cpfValidate() {
+//     const cpf = inputCpf.value;
+//     if (cpf.length === 0) {
+//         inputCpf.classList.remove('correct')
+//         inputCpf.classList.add('erro');
+//         inputCpf.setAttribute('placeholder', '* Campo obrigatório');
+//         return false;
+//     } else if (cpf.length > 11) {
+//         inputCpf.classList.remove('correct')
+//         inputCpf.classList.add('erro');
+//         alert('Excedido o número de caracteres');
+//         return false;
+//     } else if (cpf.length !== 11) {
+//         return false;
+//     } else {
+//         inputCpf.classList.remove('erro');
+//         inputCpf.classList.add('correct');
+//         return true;
+//     }
+// }
+// const inputAddress = document.querySelector('#input-address');
+// function addressValidate() {
+//     const address = inputAddress.value
+//     if (address.length === 0) {
+//         inputAddress.classList.remove('correct')
+//         inputAddress.classList.add('erro');
+//         inputAddress.setAttribute('placeholder', '* Campo obrigatório');
+//         return false;
+//     } else if (address.length > 200) {
+//         inputAddress.classList.remove('correct')
+//         inputAddress.classList.add('erro');
+//         alert('Excedido o número de caracteres');
+//         return false;
+//     } else if (address.length < 7) {
+//         return false;
+//     } else {
+//         inputAddress.classList.remove('erro');
+//         inputAddress.classList.add('correct');
+//         return true;
+//     }
+// }
+// const inputCity = document.querySelector('#input-city');
+// function cityValidate() {
+//     const city = inputCity.value
+//     if (city.length === 0) {
+//         inputCity.classList.remove('correct')
+//         inputCity.classList.add('erro');
+//         inputCity.setAttribute('placeholder', '* Campo obrigatório');
+//         return false;
+//     } else if (city.length > 28) {
+//         inputCity.classList.remove('correct')
+//         inputCity.classList.add('erro');
+//         alert('Excedido o número de caracteres');
+//         return false;
+//     } else if (city.length < 3) {
+//         return false;
+//     } else {
+//         inputCity.classList.remove('erro');
+//         inputCity.classList.add('correct');
+//         return true;
+//     }
+// }
+// const inputState = document.querySelector('#select-state');
+// function stateValidate() {
+//     if (inputState.value === inputState[0].value) {
+//         inputState.classList.remove('correct')
+//         inputState.classList.add('erro');
+//         return false;
+//     } else {
+//         inputState.classList.remove('erro');
+//         inputState.classList.add('correct');
+//         return true;
+//     }
+// }
+// const inputCurriculumSummary = document.querySelector('#curriculum-summary');
+// function curriculumSummaryValidate() {
+//     const curriculum = inputCurriculumSummary.value
+//     if (curriculum.length === 0) {
+//         inputCurriculumSummary.classList.remove('correct-txt')
+//         inputCurriculumSummary.classList.add('erro-txt');
+//         inputCurriculumSummary.setAttribute('placeholder', '* Campo obrigatório');
+//         return false;
+//     } else if (curriculum.length > 1000) {
+//         inputCurriculumSummary.classList.remove('correct-txt')
+//         inputCurriculumSummary.classList.add('erro-txt');
+//         alert('Excedido o número de caracteres');
+//         return false;
+//     } else if (curriculum.length < 50) {
+//         return false;
+//     } else {
+//         inputCurriculumSummary.classList.remove('erro-txt');
+//         inputCurriculumSummary.classList.add('correct-txt');
+//         return true;
+//     }
+// }
+// const inputOffice = document.querySelector('#input-office');
+// function officeValidate() {
+//     const office = inputOffice.value
+//     if (office.length === 0) {
+//         inputOffice.classList.remove('correct')
+//         inputOffice.classList.add('erro');
+//         inputOffice.setAttribute('placeholder', '* Campo obrigatório');
+//         return false;
+//     } else if (office.length > 40) {
+//         inputOffice.classList.remove('correct')
+//         inputOffice.classList.add('erro');
+//         alert('Excedido o número de caracteres');
+//         return false;
+//     } else if (office.length < 3) {
+//         return false;
+//     } else {
+//         inputOffice.classList.remove('erro');
+//         inputOffice.classList.add('correct');
+//         return true;
+//     }
+// }
+// const inputJobDescription = document.querySelector('#job-description');
+// function jobDescriptionValidate() {
+//     const jobDescription = inputJobDescription.value
+//     if (jobDescription.length === 0) {
+//         inputJobDescription.classList.remove('correct-txt')
+//         inputJobDescription.classList.add('erro-txt');
+//         inputJobDescription.setAttribute('placeholder', '* Campo obrigatório');
+//         return false;
+//     } else if (jobDescription.length > 500) {
+//         inputJobDescription.classList.remove('correct-txt')
+//         inputJobDescription.classList.add('erro-txt');
+//         alert('Excedido o número de caracteres');
+//         return false;
+//     } else {
+//         inputJobDescription.classList.remove('erro-txt');
+//         inputJobDescription.classList.add('correct-txt');
+//         return true;
+//     }
+// }
+// function dateOk() {
+//     if (initialDate.value.length === 0) {
+//         initialDate.classList.remove('correct')
+//         initialDate.classList.add('erro');
+//         initialDate.setAttribute('placeholder', '* Campo obrigatório');
+//         return false;
+//     } else {
+//         initialDate.classList.remove('erro');
+//         initialDate.classList.add('correct');
+//         return true;
+//     }
+// }
+// function isTrue() {
+//     if (nameValidate() &&
+//     emailValidate() &&
+//     cpfValidate() &&
+//     addressValidate() &&
+//     cityValidate() &&
+//     stateValidate() &&
+//     curriculumSummaryValidate() &&
+//     officeValidate() &&
+//     jobDescriptionValidate() &&
+//     dateOk() === true) {
+//         return true
+//     }
+//     return false;
+// }
+// inputName.addEventListener('keyup', nameValidate);
+// inputEmail.addEventListener('keyup', emailValidate);
+// inputCpf.addEventListener('keyup', cpfValidate);
+// inputAddress.addEventListener('keyup', addressValidate);
+// inputCity.addEventListener('keyup', cityValidate);
+// inputState.addEventListener('click', stateValidate);
+// inputCurriculumSummary.addEventListener('keyup', curriculumSummaryValidate);
+// inputOffice.addEventListener('keyup', officeValidate);
+// inputJobDescription.addEventListener('keyup', jobDescriptionValidate);
+// initialDate.addEventListener('focusout', dateOk);
+// const form = document.querySelector('#form');
+// const btnSubmit = document.querySelector('#button-submit');
+// btnSubmit.addEventListener('click', () => {
+//     nameValidate();
+//     emailValidate();
+//     cpfValidate();
+//     addressValidate();
+//     cityValidate();
+//     stateValidate();
+//     curriculumSummaryValidate();
+//     officeValidate();
+//     jobDescriptionValidate();
+//     dateOk();
+//     isTrue();
+//     form.addEventListener('click', (event) => {
+//         if (isTrue() === false) {
+//             event.preventDefault();
+//         }
+//     })
+// });
---
+++ bloco_6/dia_1/script.js
@@ -0,0 +1,287 @@
+// Adiciona estados dinamicamente ao campo de estados do form
+const states = ["Escolha..." , "Acre" , "Alagoas" , "Amapá" , "Amazonas" , "Bahia" , "Ceará" , "Distrito Federal" , "Espírito Santo" , "Goiás" , "Maranhão" , "Mato Grosso" , "Mato Grosso do Sul" , "Minas Gerais" , "Pará" , "Paraíba" , "Paraná" , "Pernambuco" , "Piauí" , "Rio de Janeiro" , "Rio Grande do Norte" , "Rio Grande do Sul" , "Rondônia" , "Roraima" , "Santa Catarina" , "São Paulo" , "Sergipe" , "Tocantins"];
+for (let index = 0; index < states.length; index += 1) {
+    const state = states[index]
+    const option = document.createElement('option');
+    option.value = state;
+    option.innerText = state;
+    document.getElementById('select-state').appendChild(option);
+}
+// Validadores:
+const btnSubmit = document.querySelector('#button-submit');
+const initialDate = document.querySelector('#initial-date');
+function dateValidate() {
+    const date = initialDate.value
+    if (date.length === 0) {
+        alert('Preencha o campo Data de início.')
+        return false
+    }
+    if (date.length !== 10) {
+        alert('Verifique formato da data de inicio.');
+        return false
+    }
+    let day = '';
+    let month = '';
+    let year = '';
+    let format = '';
+    for (let index = 0; index < date.length; index += 1) {
+        if (index < 2) {
+            day += date[index];
+        } else if (index === 3 || index === 4) {
+            month += date[index];
+        } else if (index > 5) {
+            year += date[index];
+        } else {
+            format += date[index];
+        }
+    }
+    if (day < 1 || day > 31) {
+        alert('Dia da data de inicio inválido.');
+        return false
+    } else if (format !== '//') {
+        alert('Verifique formato da data de inicio.');
+        return false
+    } else if (month < 1 || month > 12) {
+        alert('Mês da data de inicio inválido.');
+        return false
+    } else if (year < 0) {
+        alert('Ano da data de inicio inválido.');
+        return false
+    } else {
+        return true;
+    }
+}
+const inputName = document.querySelector('#full-name');
+function nameValidate() {
+    const fullName = inputName.value;
+    if (fullName.length === 0) {
+        inputName.classList.remove('correct')
+        inputName.classList.add('erro');
+        inputName.setAttribute('placeholder', '* Campo obrigatório');
+        return false;
+    } else if (fullName.length > 40) {
+        inputName.classList.remove('correct')
+        inputName.classList.add('erro');
+        alert('Excedido o número de caracteres');
+        return false;
+    } else if (fullName.length < 7) {
+        return false;
+    } else {
+        inputName.classList.remove('erro');
+        inputName.classList.add('correct');
+        return true;
+    }
+}
+const inputEmail = document.querySelector('#input-email');
+function emailValidate() {
+    const email = inputEmail.value;
+    if (email.length === 0) {
+        inputEmail.classList.remove('correct')
+        inputEmail.classList.add('erro');
+        inputEmail.setAttribute('placeholder', '* Campo obrigatório');
+        return false;
+    } else if (email.length > 50) {
+        inputEmail.classList.remove('correct')
+        inputEmail.classList.add('erro');
+        alert('Excedido o número de caracteres');
+        return false;
+    } else if (email.indexOf('@') === -1 || email.length < 3) {
+        return false;
+    } else {
+        inputEmail.classList.remove('erro');
+        inputEmail.classList.add('correct');
+        return true;
+    }
+}
+const inputCpf = document.querySelector('#input-cpf');
+function cpfValidate() {
+    const cpf = inputCpf.value;
+    if (cpf.length === 0) {
+        inputCpf.classList.remove('correct')
+        inputCpf.classList.add('erro');
+        inputCpf.setAttribute('placeholder', '* Campo obrigatório');
+        return false;
+    } else if (cpf.length > 11) {
+        inputCpf.classList.remove('correct')
+        inputCpf.classList.add('erro');
+        alert('Excedido o número de caracteres');
+        return false;
+    } else if (cpf.length !== 11) {
+        return false;
+    } else {
+        inputCpf.classList.remove('erro');
+        inputCpf.classList.add('correct');
+        return true;
+    }
+}
+const inputAddress = document.querySelector('#input-address');
+function addressValidate() {
+    const address = inputAddress.value
+    if (address.length === 0) {
+        inputAddress.classList.remove('correct')
+        inputAddress.classList.add('erro');
+        inputAddress.setAttribute('placeholder', '* Campo obrigatório');
+        return false;
+    } else if (address.length > 200) {
+        inputAddress.classList.remove('correct')
+        inputAddress.classList.add('erro');
+        alert('Excedido o número de caracteres');
+        return false;
+    } else if (address.length < 7) {
+        return false;
+    } else {
+        inputAddress.classList.remove('erro');
+        inputAddress.classList.add('correct');
+        return true;
+    }
+}
+const inputCity = document.querySelector('#input-city');
+function cityValidate() {
+    const city = inputCity.value
+    if (city.length === 0) {
+        inputCity.classList.remove('correct')
+        inputCity.classList.add('erro');
+        inputCity.setAttribute('placeholder', '* Campo obrigatório');
+        return false;
+    } else if (city.length > 28) {
+        inputCity.classList.remove('correct')
+        inputCity.classList.add('erro');
+        alert('Excedido o número de caracteres');
+        return false;
+    } else if (city.length < 3) {
+        return false;
+    } else {
+        inputCity.classList.remove('erro');
+        inputCity.classList.add('correct');
+        return true;
+    }
+}
+const inputState = document.querySelector('#select-state');
+function stateValidate() {
+    if (inputState.value === inputState[0].value) {
+        inputState.classList.remove('correct')
+        inputState.classList.add('erro');
+        return false;
+    } else {
+        inputState.classList.remove('erro');
+        inputState.classList.add('correct');
+        return true;
+    }
+}
+const inputCurriculumSummary = document.querySelector('#curriculum-summary');
+function curriculumSummaryValidate() {
+    const curriculum = inputCurriculumSummary.value
+    if (curriculum.length === 0) {
+        inputCurriculumSummary.classList.remove('correct-txt')
+        inputCurriculumSummary.classList.add('erro-txt');
+        inputCurriculumSummary.setAttribute('placeholder', '* Campo obrigatório');
+        return false;
+    } else if (curriculum.length > 1000) {
+        inputCurriculumSummary.classList.remove('correct-txt')
+        inputCurriculumSummary.classList.add('erro-txt');
+        alert('Excedido o número de caracteres');
+        return false;
+    } else if (curriculum.length < 50) {
+        return false;
+    } else {
+        inputCurriculumSummary.classList.remove('erro-txt');
+        inputCurriculumSummary.classList.add('correct-txt');
+        return true;
+    }
+}
+const inputOffice = document.querySelector('#input-office');
+function officeValidate() {
+    const office = inputOffice.value
+    if (office.length === 0) {
+        inputOffice.classList.remove('correct')
+        inputOffice.classList.add('erro');
+        inputOffice.setAttribute('placeholder', '* Campo obrigatório');
+        return false;
+    } else if (office.length > 40) {
+        inputOffice.classList.remove('correct')
+        inputOffice.classList.add('erro');
+        alert('Excedido o número de caracteres');
+        return false;
+    } else if (office.length < 3) {
+        return false;
+    } else {
+        inputOffice.classList.remove('erro');
+        inputOffice.classList.add('correct');
+        return true;
+    }
+}
+const inputJobDescription = document.querySelector('#job-description');
+function jobDescriptionValidate() {
+    const jobDescription = inputJobDescription.value
+    if (jobDescription.length === 0) {
+        inputJobDescription.classList.remove('correct-txt')
+        inputJobDescription.classList.add('erro-txt');
+        inputJobDescription.setAttribute('placeholder', '* Campo obrigatório');
+        return false;
+    } else if (jobDescription.length > 500) {
+        inputJobDescription.classList.remove('correct-txt')
+        inputJobDescription.classList.add('erro-txt');
+        alert('Excedido o número de caracteres');
+        return false;
+    } else {
+        inputJobDescription.classList.remove('erro-txt');
+        inputJobDescription.classList.add('correct-txt');
+        return true;
+    }
+}
+function dateOk() {
+    if (initialDate.value.length === 0) {
+        initialDate.classList.remove('correct')
+        initialDate.classList.add('erro');
+        initialDate.setAttribute('placeholder', '* Campo obrigatório');
+        return false;
+    } else {
+        initialDate.classList.remove('erro');
+        initialDate.classList.add('correct');
+        return true;
+    }
+}
+function isTrue() {
+    if (nameValidate() &&
+    emailValidate() &&
+    cpfValidate() &&
+    addressValidate() &&
+    cityValidate() &&
+    stateValidate() &&
+    curriculumSummaryValidate() &&
+    officeValidate() &&
+    jobDescriptionValidate() &&
+    dateOk() === true) {
+        return true
+    }
+    return false;
+}
+inputName.addEventListener('keyup', nameValidate);
+inputEmail.addEventListener('keyup', emailValidate);
+inputCpf.addEventListener('keyup', cpfValidate);
+inputAddress.addEventListener('keyup', addressValidate);
+inputCity.addEventListener('keyup', cityValidate);
+inputState.addEventListener('click', stateValidate);
+inputCurriculumSummary.addEventListener('keyup', curriculumSummaryValidate);
+inputOffice.addEventListener('keyup', officeValidate);
+inputJobDescription.addEventListener('keyup', jobDescriptionValidate);
+initialDate.addEventListener('focusout', dateOk);
+const form = document.querySelector('#form');
+btnSubmit.addEventListener('click', () => {
+    nameValidate();
+    emailValidate();
+    cpfValidate();
+    addressValidate();
+    cityValidate();
+    stateValidate();
+    curriculumSummaryValidate();
+    officeValidate();
+    jobDescriptionValidate();
+    dateOk();
+    isTrue();
+    form.addEventListener('click', (event) => {
+        if (isTrue() === false) {
+            event.preventDefault();
+        }
+    })
+});
