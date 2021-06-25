bbb2bc9139344d1b385425de97eaae40ff877828
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2021-06-25 12:12:46 -0300

    adiciona PropTypes ao componente Button

--- bloco_12/dia_01/src/components/Button.js
+++ bloco_12/dia_01/src/components/Button.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Button extends React.Component {
     render() {
         const props = this.props;
         return (
-            <button type={props.btnType} className={props.btnClass} onClick={props.btnFunc}>{props.btnText}</button>
+            <button type={props.btnType} className={props.btnClass} onClick={props.btnFunc} value={props.btnValue}>{props.btnText}</button>
         )
     }
+}
+
+Button.propTypes = {
+    btnType: PropTypes.string.isRequired,
+    btnClass: PropTypes.string,
+    btnFunc: PropTypes.func,
+    btnValue: PropTypes.string,
+    btnText: PropTypes.string.isRequired,
+}
+
+Button.defaultProps = {
+    btnClass: '',
+    btnFunc: '',
+    btnValue: '',
 }
 
 export default Button;
