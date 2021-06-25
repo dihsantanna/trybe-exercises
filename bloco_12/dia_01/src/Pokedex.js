593815a69f532e7fdc9b0c512194ec18b72d8d2b
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2021-06-25 01:08:25 -0300

    adiciona botoes de filtros por tipo e implementa logica para funcionamento

--- bloco_12/dia_01/src/Pokedex.js
+++ bloco_12/dia_01/src/Pokedex.js
@@ -6,44 +6,67 @@ import './App.css';
 class Pokedex extends React.Component {
     constructor() {
         super()
+
         this.state = { 
             pokeIndex: 0,
+            pokeType: 'Psychic'
          };
+
+        this.typeChange = this.typeChange.bind(this);
+
+        this.pokeFilter = this.pokeFilter.bind(this);
+
         this.indexPokemon = this.indexPokemon.bind(this);
     };
 
+    typeChange(e) {
+        const event = e.target.innerText.contains !== this.state.pokeType;
+        if (event) {
+            return this.setState({ pokeType: e.target.innerText });
+        }
+    }
+    
+    pokeFilter(pokemons) {
+        return pokemons.filter((pokemon) => pokemon.type === this.state.pokeType);
+    }
+    
     indexPokemon(pokemons) {
         const index = this.state.pokeIndex;
+        const indexFilter = this.pokeFilter(pokemons).length - 1;
 
-        if (index === (pokemons.length - 1)) return this.setState({ pokeIndex: 0 });
+        if (index === indexFilter) return this.setState({ pokeIndex: 0 });
 
         this.setState((before, _props) => ({ pokeIndex: before.pokeIndex + 1 }))
     };
-
+    
     pokemonRender(pokemons) {
         const index = this.state.pokeIndex;
-
-        const { name, type, averageWeight, image, id } = pokemons[index];
-
+        
+        const filterPokemon = this.pokeFilter(pokemons)
+        
+        const { name, type, averageWeight, image, id } = filterPokemon[index];
+        
         return <Pokemon
-                name={name}
-                type={type}
-                weight={averageWeight.value}
-                unity={averageWeight.measurementUnit}
-                src={image} alt={`image-${name}`}
-                key={id}/>;
-    
+        name={name}
+        type={type}
+        weight={averageWeight.value}
+        unity={averageWeight.measurementUnit}
+        src={image} alt={`image-${name}`}
+        key={id}/>;
+        
     };
 
     render() {
         const { pokemons } = this.props
         return (
-            <div className="main">
-                <section className="pokedex">
-                    {this.pokemonRender(pokemons)}
-                    <Button btnType='button' btnClass='btn-next' btnFunc={() => this.indexPokemon(pokemons)} btnText='Next Pokemon'/>
-                </section>
-            </div>
+            <section className="pokedex">
+                {this.pokemonRender(pokemons)}
+                <div className="poke-type">
+                <Button btnType='button' btnClass='btn-psychic' btnFunc={(event) => this.typeChange(event)} btnText='Psychic'/>
+                <Button btnType='button' btnClass='btn-fire' btnFunc={(event) => this.typeChange(event)} btnText='Fire'/>
+                </div>
+                <Button btnType='button' btnClass='btn-next' btnFunc={() => this.indexPokemon(pokemons)} btnText='Next Pokemon'/>
+            </section>
         )
     }
 }
