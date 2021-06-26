18dd57a4f748cc318ae1c869bb3128be4ee06ae5
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2021-06-26 18:42:47 -0300

    commit inicial: cria diretorio para exercicios e inicia criaçao de componente Form

---
+++ bloco_12/dia_02/my-form-2.0/src/components/Form.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <form className="form">
+        <legend className="legend-form">Currículo</legend>
+      </form>
+    );
+  }
+}
+
+export default App;
