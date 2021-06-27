90fe35e5460311066bfd73bd75278b0079ade496
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2021-06-27 00:05:41 -0300

    cria componente InputCPF para adiçao de campo CPF e cria valiadaçoes para o campo

--- bloco_12/dia_02/my-form-2.0/src/components/Form.js
+++ bloco_12/dia_02/my-form-2.0/src/components/Form.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import InputName from './InputName';
 import InputEmail from './InputEmail';
+import InputCPF from './InputCPF';
 import '../App.css';
 
 class Form extends Component {
@@ -9,6 +10,7 @@ class Form extends Component {
     this.state = {
       name: '',
       email: '',
+      cpf: '',
       formWithError: true,
     };
     this.handleChange = this.handleChange.bind(this);
@@ -17,15 +19,18 @@ class Form extends Component {
       fieldCheck: {
         nameCheck: (name) => name.match(/(\s|[A-Za-z]|[áãéêíóôõú]|'){0,40}/gi)[0],
         emailCheck: (email) => email.match(/(\w|\d|\.|@|-){0,50}/gi)[0],
+        cpfCheck: (cpf) => cpf.match(/\d{0,11}/gi)[0],
       },
       checkForm: () => {
-        const { name, email } = this.state;
+        const { name, email, cpf } = this.state;
 
         const nameMin = 7;
+        const cpfLength = 11;
 
         const errorCheck = [
           name.length <= nameMin,
           !email.match(/\b[a-z]+(\.|-|\w|\d)*?@[a-z]+\.[a-z]{2,3}(\.br)?$/i),
+          cpf.length < cpfLength,
         ];
 
         const isOk = errorCheck.every((error) => error !== true);
@@ -42,8 +47,8 @@ class Form extends Component {
   }
 
   render() {
-    const { name, email, formWithError } = this.state;
-    const { nameCheck, emailCheck } = this.checkAll.fieldCheck;
+    const { name, email, cpf, formWithError } = this.state;
+    const { nameCheck, emailCheck, cpfCheck } = this.checkAll.fieldCheck;
     return (
       <form className="form">
         <legend className="legend-form">Currículo</legend>
@@ -58,6 +63,12 @@ class Form extends Component {
           <InputEmail
             inptName="email"
             inptValue={ !email ? email : emailCheck(email).toLowerCase() }
+            inptFuncChange={ this.handleChange }
+          />
+
+          <InputCPF
+            inptName="cpf"
+            inptValue={ !cpf ? cpf : cpfCheck(cpf) }
             inptFuncChange={ this.handleChange }
           />
         </fieldset>
---
+++ bloco_12/dia_02/my-form-2.0/src/components/InputCPF.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Input from './Input';
+import '../App.css';
+
+class InputCPF extends Component {
+  render() {
+    const { inptValue, inptFuncChange, inptName } = this.props;
+
+    return (
+      <Input
+        inptTitle="CPF : "
+        inptName={ inptName }
+        inptClass="input-cpf"
+        inptId="input-cpf"
+        inptValue={ inptValue }
+        inptFuncChange={ inptFuncChange }
+      />
+    );
+  }
+}
+
+InputCPF.propTypes = {
+  inptName: PropTypes.string.isRequired,
+  inptValue: PropTypes.string,
+  inptFuncChange: PropTypes.func,
+};
+
+InputCPF.defaultProps = {
+  inptValue: '',
+  inptFuncChange: () => {},
+};
+
+export default InputCPF;
