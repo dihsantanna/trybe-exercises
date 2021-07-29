e025108e303c87ef17ba603abc6c0cecd0309473
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2021-07-29 12:57:58 -0300

    complementa reducers com as condições types

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,31 @@
-function reducer(state, action) {
+import { IS_FETCHING, FETCH_OK, FETCH_ERROR } from "../action";
+
+const INITIAL_STATE = {
+  character: {},
+  loading: false,
+  error: '',
+}
+
+function reducer(state = INITIAL_STATE, action) {
   switch(action.type) {
+    case IS_FETCHING:
+      return {
+        ...state,
+        loading: true,
+        error: '',
+      };
+    case FETCH_OK:
+      return {
+        character: {...action.character},
+        loading: false,
+        error: '',
+      };
+    case FETCH_ERROR:
+      return {
+        character: {},
+        loading: false,
+        error: action.error,
+      }
     default:
       return state;
   }
