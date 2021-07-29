73ba5e65a23eb2c0e4f1348877543ab92198113f
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2021-07-29 11:54:09 -0300

    implementa logica com redux-thunk para requisição da api

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,21 @@
 export const REQUEST_API = 'REQUEST_API';
 export const GET_PICTURE = 'GET_PICTURE';
 
+const API_URL = 'https://aws.random.cat/meow'
+
 export const requestAPI = () => ({ type: REQUEST_API });
 
 export const getPicture = (data) => ({ type: GET_PICTURE, data });
 
 export function fetchAPI() {
-  // Desenvolva aqui o código da action assíncrona
+  return async (dispatch) => {
+    dispatch(requestAPI());
+    try {
+      const response = await fetch(API_URL);
+      const cat = await response.json();
+      return dispatch(getPicture(cat));
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
